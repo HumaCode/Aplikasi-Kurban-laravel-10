ed25e21e9ccce38d4b965d43ccab647a71c74a96
humaidi <humaidizakaria1998@gmail.com>
2023-07-17 10:12:29 +0700

    Perbaikan Tampilan Kas Masjid Untuk Laporan

--- resources/views/kas_form.blade.php
+++ resources/views/kas_form.blade.php
@@ -1,80 +1,72 @@
 @extends('layouts.app_adminkit')
 
 @section('content')
-    <div class="container-fluid p-0">
-        <h1 class="h3 mb-3">{{ $subtitle }}</h1>
+<div class="container-fluid p-0">
+    <h1 class="h3 mb-3">{{ $subtitle }}</h1>
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
 
-                    <div class="card-body">
+                <div class="card-body">
 
-                        <div class="alert alert-primary">
-                            <strong class="fs-3">Saldo akhir saat ini adalah :
-                                {{ formatRupiah($saldoAkhir, true) }}</strong>
-                        </div>
-
-                        {!! Form::model($kas, [
-                            'route' => isset($kas->id) ? ['kas.update', $kas->id] : 'kas.store',
-                            'method' => isset($kas->id) ? 'PUT' : 'POST',
-                        ]) !!}
-
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <div>
-                                    {!! Form::label('tanggal', 'Tanggal', ['class' => 'form-label']) !!}
-                                    {!! Form::date('tanggal', $kas->tanggal ?? now(), ['class' => 'form-control', $disable]) !!}
-                                    <span class="text-danger">{{ $errors->first('tanggal') }}</span>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div>
-                                    {!! Form::label('kategori', 'Kategori (optional)', ['class' => 'form-label']) !!}
-                                    {!! Form::text('kategori', null, ['class' => 'form-control']) !!}
-                                    <span class="text-danger">{{ $errors->first('kategori') }}</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mb-3">
-                            {!! Form::label('keterangan', 'Keterangan', ['class' => 'form-label']) !!}
-                            {!! Form::textarea('keterangan', null, [
-                                'class' => 'form-control',
-                                'placeholder' => 'Masukkan Keterangan',
-                            ]) !!}
-                            <span class="text-danger">{{ $errors->first('keterangan') }}</span>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <div>
-                                    {!! Form::label('jenis', 'Jenis', ['class' => 'form-label']) !!}
-                                    <div class="form-check">
-                                        {!! Form::radio('jenis', 'masuk', 1, ['class' => 'form-check-input', 'id' => 'jenis_masuk', $disable]) !!}
-                                        {!! Form::label('jenis_masuk', 'Pemasukan', ['class' => 'form-check-label']) !!}
-                                    </div>
-                                    <div class="form-check">
-                                        {!! Form::radio('jenis', 'keluar', null, ['class' => 'form-check-input', 'id' => 'jenis_keluar', $disable]) !!}
-                                        {!! Form::label('jenis_keluar', 'Pengeluaran', ['class' => 'form-check-label']) !!}
-                                    </div>
-                                    <span class="text-danger">{{ $errors->first('jenis') }}</span>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div>
-                                    {!! Form::label('jumlah', 'Jumlah', ['class' => 'form-label']) !!}
-                                    {!! Form::text('jumlah', null, ['class' => 'form-control rupiah', 'placeholder' => 'Masukkan Jumlah']) !!}
-                                    <span class="text-danger">{{ $errors->first('jumlah') }}</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        {!! Form::submit('SIMPAN DATA', ['class' => 'btn btn-primary']) !!}
-                        {!! Form::close() !!}
+                    <div class="alert alert-primary">
+                        <strong class="fs-3">Saldo akhir saat ini adalah :
+                            {{ formatRupiah($saldoAkhir, true) }}</strong>
                     </div>
+
+                    {!! Form::model($kas, [
+                    'route' => isset($kas->id) ? ['kas.update', $kas->id] : 'kas.store',
+                    'method' => isset($kas->id) ? 'PUT' : 'POST',
+                    ]) !!}
+
+                    <div class="mb-3">
+                        {!! Form::label('tanggal', 'Tanggal', ['class' => 'form-label']) !!}
+                        {!! Form::date('tanggal', $kas->tanggal ?? now(), ['class' => 'form-control', $disable]) !!}
+                        <span class="text-danger">{{ $errors->first('tanggal') }}</span>
+                    </div>
+
+                    <div class="mb-3">
+                        {!! Form::label('keterangan', 'Keterangan', ['class' => 'form-label']) !!}
+                        {!! Form::textarea('keterangan', null, [
+                        'class' => 'form-control',
+                        'placeholder' => 'Masukkan Keterangan',
+                        ]) !!}
+                        <span class="text-danger">{{ $errors->first('keterangan') }}</span>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <div>
+                                {!! Form::label('jenis', 'Jenis', ['class' => 'form-label']) !!}
+                                <div class="form-check">
+                                    {!! Form::radio('jenis', 'masuk', 1, ['class' => 'form-check-input', 'id' =>
+                                    'jenis_masuk', $disable]) !!}
+                                    {!! Form::label('jenis_masuk', 'Pemasukan', ['class' => 'form-check-label']) !!}
+                                </div>
+                                <div class="form-check">
+                                    {!! Form::radio('jenis', 'keluar', null, ['class' => 'form-check-input', 'id' =>
+                                    'jenis_keluar', $disable]) !!}
+                                    {!! Form::label('jenis_keluar', 'Pengeluaran', ['class' => 'form-check-label']) !!}
+                                </div>
+                                <span class="text-danger">{{ $errors->first('jenis') }}</span>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div>
+                                {!! Form::label('jumlah', 'Jumlah', ['class' => 'form-label']) !!}
+                                {!! Form::text('jumlah', null, ['class' => 'form-control rupiah', 'placeholder' =>
+                                'Masukkan Jumlah']) !!}
+                                <span class="text-danger">{{ $errors->first('jumlah') }}</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    {!! Form::submit('SIMPAN DATA', ['class' => 'btn btn-primary']) !!}
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/kas_index.blade.php
+++ resources/views/kas_index.blade.php
@@ -10,129 +10,142 @@
 </style>
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="d-flex justify-content-between align-items-center mb-4">
-                    <h2>{{ strtoupper($title) }}</h2>
-                    <a href="{{ route('kas.create') }}" class="btn btn-primary">Tambah Kas</a>
-                </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="d-flex justify-content-between align-items-center mb-4">
+                <h2>{{ strtoupper($title) }}</h2>
+                <a href="{{ route('kas.create') }}" class="btn btn-primary">Tambah Kas</a>
+            </div>
 
-                {!! Form::open([
-                    'url' => url()->current(),
-                    'method' => 'GET',
-                    'class' => 'row row-cols-lg-auto g-3 align-items-center',
-                ]) !!}
+            {!! Form::open([
+            'url' => url()->current(),
+            'method' => 'GET',
+            'class' => 'row row-cols-lg-auto g-3 align-items-center',
+            ]) !!}
 
-                <div class="col-12">
-                    {{-- <label for="inlineFormInputGroupUsername">Tanggal Transaksi</label> --}}
-                    <div class="input-group">
-                        {!! Form::date('tanggal', null, [
-                            'class' => 'form-control',
-                            'id' => 'inlineFormInputGroupUsername',
-                        ]) !!}
-                    </div>
-                </div>
-
-                <div class="col-12">
-                    {{-- <label for="text">Keterangan Transaksi</label> --}}
-                    {!! Form::text('q', request('q'), [
-                        'class' => 'form-control',
-                        'id' => 'text',
-                        'placeholder' => 'Keterangan Transaksi',
+            <div class="col-12">
+                {{-- <label for="inlineFormInputGroupUsername">Tanggal Transaksi</label> --}}
+                <div class="input-group">
+                    {!! Form::date('tanggal_mulai', request('tanggal_mulai'), [
+                    'class' => 'form-control',
+                    'id' => 'inlineFormInputGroupUsername',
                     ]) !!}
                 </div>
-
-                <div class="col-12 ">
-                    <button type="submit" class="btn btn-primary">Cari</button>
-                </div>
-                {!! Form::close() !!}
-
-                @if (count($kases) > 0)
-                    <div class="card">
-                        <div class="card-body">
-
-                            <div class="table-responsive">
-                                <table class="{{ config('app.table-style') }}">
-                                    <thead class="text-center table-info">
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Tanggal</th>
-                                            <th>Dibuat</th>
-                                            <th>Kategori</th>
-                                            <th>Keterangan</th>
-                                            <th>Pemasukan</th>
-                                            <th>Pengeluaran</th>
-                                            <th width="5%">Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($kases as $item)
-                                            <tr>
-                                                <td class="text-center">{{ $loop->iteration }}.</td>
-                                                <td>{{ $item->tanggal->translatedFormat('d-m-Y') }}</td>
-                                                <td>{{ $item->createdBy->name }}</td>
-                                                <td class="text-center">{{ $item->kategori ?? 'umum' }}</td>
-                                                <td>{{ $item->keterangan }}</td>
-                                                <td class="text-end">
-                                                    {{ $item->jenis == 'masuk' ? formatRupiah($item->jumlah, true) : '-' }}
-                                                </td>
-                                                <td class="text-end">
-                                                    {{ $item->jenis == 'keluar' ? formatRupiah($item->jumlah, true) : '-' }}
-                                                </td>
-                                                <td class="text-center">
-
-                                                    <a href="{{ route('kas.edit', $item->id) }}"
-                                                        class="btn btn-success btn-sm my-2"><i
-                                                            class="fas fa-pencil-alt"></i></a>
-
-                                                    {!! Form::open([
-                                                        'method' => 'DELETE',
-                                                        'route' => ['kas.destroy', $item->id],
-                                                        'style' => 'display:inline',
-                                                    ]) !!}
-
-                                                    @csrf
-
-                                                    <button type="submit" class="btn btn-danger btn-sm"
-                                                        onclick="return confirm('Apakah Anda yakin ingin menghapus data kas ini?')"><i
-                                                            class="fas fa-trash"></i></button>
-                                                    {!! Form::close() !!}
-
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-
-                                    <tfoot>
-                                        <tr class="fw-bold">
-                                            <td colspan="5">TOTAL</td>
-                                            <td class="text-end">
-                                                {{ formatRupiah($totalPemasukan, true) }}
-                                            </td>
-                                            <td class="text-end">
-                                                {{ formatRupiah($totalPengeluaran, true) }}
-                                            </td>
-                                            <td></td>
-                                        </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
-
-                            <div class="alert alert-info text-end">
-                                <strong class="fs-3">Saldo Akhir : {{ formatRupiah($saldoAkhir, true) }}</strong>
-                            </div>
-
-                            {{ $kases->links() }}
-                        </div>
-                    </div>
-                @else
-                    <div class="text-center">
-                        <h3 class=" mt-3">Tidak ada data kas.</h3>
-                        <img src="{{ asset('images/empty.gif') }}" class="lingkaran" alt="">
-                    </div>
-                @endif
             </div>
+
+            <div class="col-12">
+                {{-- <label for="inlineFormInputGroupUsername">Tanggal Transaksi</label> --}}
+                <div class="input-group">
+                    {!! Form::date('tanggal_selesai', request('tanggal_selesai'), [
+                    'class' => 'form-control',
+                    'id' => 'inlineFormInputGroupUsername',
+                    ]) !!}
+                </div>
+            </div>
+
+            <div class="col-12">
+                {{-- <label for="text">Keterangan Transaksi</label> --}}
+                {!! Form::text('q', request('q'), [
+                'class' => 'form-control',
+                'id' => 'text',
+                'placeholder' => 'Keterangan Transaksi',
+                ]) !!}
+            </div>
+
+            <div class="col-12 ">
+                <button type="submit" class="btn btn-primary">Cari</button>
+            </div>
+            {!! Form::close() !!}
+
+            @if (count($kases) > 0)
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="table-responsive">
+                        <table class="{{ config('app.table-style') }}">
+                            <thead class="text-center table-info">
+                                <tr>
+                                    <th>No</th>
+                                    <th width="10%">Tanggal</th>
+                                    <th>Dibuat</th>
+                                    <th>Keterangan</th>
+                                    <th>Pemasukan</th>
+                                    <th>Pengeluaran</th>
+                                    <th width="5%">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($kases as $item)
+                                <tr>
+                                    <td class="text-center"><small>{{ $loop->iteration }}.</small></td>
+                                    <td><small>{{ $item->tanggal->translatedFormat('d-m-Y') }}</small></td>
+                                    <td><small>{{ $item->createdBy->name }}</small></td>
+                                    <td>
+                                        <small>{{ $item->keterangan }}</small>
+                                    </td>
+                                    <td class="text-end">
+                                        <small>{{ $item->jenis == 'masuk' ? formatRupiah($item->jumlah, true) : '-'
+                                            }}</small>
+                                    </td>
+                                    <td class="text-end">
+                                        <small>{{ $item->jenis == 'keluar' ? formatRupiah($item->jumlah, true) : '-'
+                                            }}</small>
+                                    </td>
+                                    <td class="text-center">
+
+                                        <a href="{{ route('kas.edit', $item->id) }}"
+                                            class="btn btn-success btn-sm my-2"><i class="fas fa-pencil-alt"></i></a>
+
+                                        {!! Form::open([
+                                        'method' => 'DELETE',
+                                        'route' => ['kas.destroy', $item->id],
+                                        'style' => 'display:inline',
+                                        ]) !!}
+
+                                        @csrf
+
+                                        <button type="submit" class="btn btn-danger btn-sm"
+                                            onclick="return confirm('Apakah Anda yakin ingin menghapus data kas ini?')"><i
+                                                class="fas fa-trash"></i></button>
+                                        {!! Form::close() !!}
+
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                            <tfoot>
+                                <tr class="fw-bold">
+                                    <td colspan="5">TOTAL</td>
+                                    <td class="text-end">
+                                        <small class="font-weight-bold">{{ formatRupiah($totalPemasukan, true)
+                                            }}</small>
+                                    </td>
+                                    <td class="text-end">
+                                        <small class="font-weight-bold">{{ formatRupiah($totalPengeluaran, true)
+                                            }}</small>
+                                    </td>
+                                    <td></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+
+                    <div class="alert alert-info text-end">
+                        <strong class="fs-3">Saldo Akhir : {{ formatRupiah($saldoAkhir, true) }}</strong>
+                    </div>
+
+                    {{ $kases->links() }}
+                </div>
+            </div>
+            @else
+            <div class="text-center">
+                <h3 class=" mt-3">Tidak ada data kas.</h3>
+                <img src="{{ asset('images/empty.gif') }}" class="lingkaran" alt="">
+            </div>
+            @endif
         </div>
     </div>
+</div>
 @endsection
