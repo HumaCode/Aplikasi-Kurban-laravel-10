a3c398e8a5136f026706096248567183485d31c0
HumaCode <HumaCode>
2023-07-25 19:53:26 +0700

    Membuat Slug dengan library spatie sluggable

--- app/Models/Masjid.php
+++ app/Models/Masjid.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Masjid extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     protected $guarded = [];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('nama')
+            ->saveSlugsTo('slug');
+    }
 }
